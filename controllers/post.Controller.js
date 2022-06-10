3df3a1e5a05d72a3a094179700034b4c41625942
bintang_editya <bintangfarhandika@gmail.com>
2022-06-10 16:04:15 +0700

    menambahkan komentar

--- controllers/post.Controller.js
+++ controllers/post.Controller.js
@@ -5,6 +5,7 @@ module.exports = {
         const posts = await Post.findAll();
         return res.render("post/index", { posts });
     },
+    // test
     create: async (req, res) => {
         return res.render("post/create");
     },
