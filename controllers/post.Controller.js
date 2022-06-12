1ab2e49740059e386150f452460dc2b2a1cabdb2
nurhafidz <nurhafidzfaizal03@gmail.com>
2022-06-12 14:27:05 +0700

    update and delete

--- controllers/post.Controller.js
+++ controllers/post.Controller.js
@@ -3,8 +3,6 @@ const Post = require("../models/Post");
 module.exports = {
     // fun index
     index: async (req, res) => {
-        //Test 2
-        //Test 4
         //Get All Posts
         const posts = await Post.findAll();
         return res.render("post/index", { posts });
@@ -21,5 +19,50 @@ module.exports = {
 
         return res.redirect("/post");
         // return res.render("post/create");
+    },
+
+    // this function for edit page
+    edit: async (req, res) => {
+        // this for get id from paramereter url
+        const id = req.params.id;
+
+        // this for get data form database with id
+        const post = await Post.findByPk(id);
+
+        return res.render("post/edit", { post });
+    },
+
+    // this function for update page
+    update: async (req, res) => {
+        const id = req.params.id;
+
+        // this for update data to database with id
+        await Post.update(
+            {
+                title: req.body.title,
+                content: req.body.content,
+            },
+            {
+                where: {
+                    id: id,
+                },
+            }
+        );
+
+        return res.redirect("/post");
+    },
+
+    // this function for delete data
+    destroy: async (req, res) => {
+        const id = req.params.id;
+
+        // this for delete field in database using id
+        await Post.destroy({
+            where: {
+                id: id,
+            },
+        });
+
+        return res.redirect("/post");
     },
 };
--- routes/index.route.js
+++ routes/index.route.js
@@ -9,5 +9,8 @@ router.get("/", indexController.index);
 router.get("/post", postController.index);
 router.get("/post/create", postController.create);
 router.post("/post/store", postController.store);
+router.get("/post/:id/edit", postController.edit); // :id -> for getting the id of the post
+router.post("/post/:id/update", postController.update);
+router.get("/post/:id/delete", postController.destroy);
 
 module.exports = router;
